6afbd08da22a1c069789b97f288c434be01dbc13
g0dgar1cc <ting@s.sfusd.edu>
2023-04-07 15:33:52 -0700

    added code for game over

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -7,7 +7,7 @@ public class Target : MonoBehaviour
     private Rigidbody targetRb;
     private GameManager gameManager;
     private float minSpeed = 12;
-    private float maxSpeed = 16;
+    private float maxSpeed = 13;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
@@ -35,14 +35,22 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-        gameManager.UpdateScore(pointValue);
+        if(gameManager.isGameActive)
+        {
+          Destroy(gameObject);
+          Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+          gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager. GameOver();
+        }
     }
     
     Vector3 RandomForce()
